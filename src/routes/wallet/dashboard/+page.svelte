3a3f12fb8775961cd75c38f8275aadfe66013167
knugen <fruktstav@gmail.com>
2022-12-14 20:26:51 +0100

    Added fiat value

--- src/routes/wallet/dashboard/+page.svelte
+++ src/routes/wallet/dashboard/+page.svelte
@@ -1,10 +1,26 @@
 <script>
   import { fade } from "svelte/transition";
+  import { wallet } from "$lib/stores/wallet.js";
+  import { fiat } from "$lib/stores/fiat.js";
+
+  $:fiatBalance = "$" + ($wallet.balance[0] * $fiat / 100000).toFixed(5);
+
+  $:console.log($fiat)
+  $:console.log($wallet.balance[0])
+
 </script>
 
 <div class="header">
   <h3 in:fade>Dashboard</h3>
 </div>
+
+<div class="wrapper">
+  <div class="card-wrapper">
+    <h3>Fiat value</h3>
+    <p>{fiatBalance}</p>
+  </div>
+</div>
+
 
 <style lang="scss">
   .header {
@@ -15,5 +31,8 @@
     min-height: 60px;
     border-bottom: 1px solid var(--border-color);
     padding: 0 2rem 0 2rem
+  }
+  .card-wrapper {
+    margin: 10px;
   }
 </style>
