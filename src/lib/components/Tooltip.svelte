b2ba26cf4661168013253b078b648ffc6ae748b3
appelskrutt34 <ebba1430@hotmail.com>
2023-04-07 19:50:45 +0200

    added tooltip to nav buttons

--- src/lib/components/Tooltip.svelte
+++ src/lib/components/Tooltip.svelte
@@ -1,42 +1,49 @@
 <script>
-	export let title = '';
-	let isHovered = false;
-	let x;
-	let y;
+  export let title = '';
+  export let stylized = false;
 
-	function mouseOver(event) {
-		isHovered = true;
-		x = event.pageX + 5;
-		y = event.pageY + 5;
-	}
-	function mouseMove(event) {
-		x = event.pageX + 5;
-		y = event.pageY + 5;
-	}
-	function mouseLeave() {
-		isHovered = false;
-	}
+  let isHovered = false;
+  let x;
+  let y;
+
+  function mouseOver(event) {
+    isHovered = true;
+    x = event.pageX + 5;
+    y = event.pageY + 5;
+  }
+  function mouseMove(event) {
+    x = event.pageX + 5;
+    y = event.pageY + 5;
+  }
+  function mouseLeave() {
+    isHovered = false;
+  }
 </script>
 
-<div
-	on:mouseover={mouseOver}
-  on:mouseleave={mouseLeave}
-	on:mousemove={mouseMove}>
-	<slot />
+<div on:mouseover={mouseOver} on:mouseleave={mouseLeave} on:mousemove={mouseMove}>
+  <slot />
 </div>
 
 {#if isHovered}
-	<div style="top: {y}px; left: {x}px;" class="tooltip">{title}</div>
+  <div style="top: {y}px; left: {x}px;" class={stylized ? 'tooltip-stylized' : 'tooltip'}>{title}</div>
 {/if}
 
 <style>
-	.tooltip {
-		border: 1px solid #ddd;
-		box-shadow: 1px 1px 1px #ddd;
-		background: white;
-		border-radius: 4px;
-		padding: 4px;
-		position: absolute;
+  .tooltip {
+    border: 1px solid #ddd;
+    box-shadow: 1px 1px 1px #ddd;
+    background: white;
+    border-radius: 4px;
+    padding: 4px;
+    position: absolute;
     color: black;
-	}
+  }
+  .tooltip-stylized {
+    border-radius: 4px;
+    padding: 6px;
+    position: absolute;
+    border: 1px solid var(--text-color);
+    color: var(--text-color);
+    background-color: var(--backgound-color);
+  }
 </style>
