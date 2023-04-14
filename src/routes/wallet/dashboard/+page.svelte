73da5bafc74264106a5003164dcfe8d2b38b734f
appelskrutt34 <ebba1430@hotmail.com>
2023-04-14 19:14:10 +0200

    new chart

--- src/routes/wallet/dashboard/+page.svelte
+++ src/routes/wallet/dashboard/+page.svelte
@@ -3,21 +3,80 @@
   import { wallet } from '$lib/stores/wallet.js';
   import { fiat } from '$lib/stores/fiat.js';
   import { onMount } from 'svelte';
-  import { LinkedChart, LinkedLabel } from 'svelte-tiny-linked-charts';
 
-  const MAX_PAGES = 10;
+  const MAX_PAGES = 5;
   var incommingTransactions = [];
   var outgoingTransactions = [];
-  var outgoingDates = [];
-  var incommingDates = [];
-  var showIncommingTransactions = true;
+  var dates = [];
+  var chart;
 
   $: fiatBalance = '$' + (($wallet.balance[0] * $fiat) / 100000).toFixed(5);
 
   onMount(async () => {
     await formatTransactions();
+    await renderchart();
   });
 
+  async function renderchart() {
+    var options = {
+      series: [
+        { data: incommingTransactions, name: 'Incomming' },
+        { data: outgoingTransactions, name: 'Outgoing' },
+      ],
+      labels: dates,
+      chart: {
+        type: 'bar',
+        height: '350',
+        width: '100%',
+        toolbar: {
+          show: false,
+        },
+      },
+      colors: ['#0eff6b', '#5a8bdb'],
+      plotOptions: {
+        bar: {
+          horizontal: false,
+          borderRadius: 2,
+          borderRadiusApplication: 'end',
+        },
+      },
+      fill: {
+        type: 'solid',
+      },
+      tooltip: {
+        enabled: true,
+      },
+      grid: {
+        show: false,
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      legend: {
+        show: true,
+      },
+      xaxis: {
+        labels: {
+          show: false,
+        },
+        axisTicks: {
+          show: false,
+        },
+      },
+      yaxis: {
+        opposite: true,
+        labels: {
+          formatter: (value) => {
+            return value + ' xkr';
+          },
+        },
+      },
+    };
+
+    const ApexCharts = (await import('apexcharts')).default;
+    chart = new ApexCharts(document.querySelector('#transactions-chart'), options);
+    chart.render();
+  }
   async function getTransactions(transactions, pageNum) {
     let startIndex = pageNum * 10;
     let txs = await window.api.getTransactions(startIndex);
@@ -39,23 +98,14 @@
     for (let i = 0; i < transactions.length; i++) {
       let value = transactions[i].amount / 100000;
       let date = new Date(transactions[i].time * 1000).toUTCString();
+      dates.push(date);
 
       if (value >= 0) {
-        let existingDateIndex = incommingDates.findIndex((d) => d == date);
-        if (existingDateIndex > -1) {
-          incommingTransactions[existingDateIndex] += value;
-        } else {
-          incommingTransactions.push(value);
-          incommingDates.push(date);
-        }
+        incommingTransactions.push(value);
+        outgoingTransactions.push(0);
       } else {
-        let existingDateIndex = outgoingDates.findIndex((d) => d == date);
-        if (existingDateIndex > -1) {
-          outgoingTransactions[existingDateIndex] += value * -1;
-        } else {
-          outgoingTransactions.push(value * -1);
-          outgoingDates.push(date);
-        }
+        outgoingTransactions.push(value * -1);
+        incommingTransactions.push(0);
       }
     }
 
@@ -63,12 +113,8 @@
     incommingTransactions = incommingTransactions.map((t) => t.toFixed(3));
     outgoingTransactions.reverse();
     incommingTransactions.reverse();
-    incommingDates.reverse();
-    outgoingDates.reverse();
-    incommingDates = incommingDates;
+    dates.reverse();
   }
-
-  $: fiatBalance = '$' + (($wallet.balance[0] * $fiat) / 100000).toFixed(5);
 </script>
 
 <div class="header">
@@ -76,80 +122,10 @@
 </div>
 
 <div class="wrapper">
-  <div class="card-wrapper">
+  <div style="margin-top: 15px; padding: 0 2rem 0 2rem">
     <h3>Fiat value</h3>
     <p>{fiatBalance}</p>
-  </div>
-  <div style="margin-top: 10px; position: relative">
-    {#if incommingDates.length > 0}
-      <div style="text-align: center; margin-bottom: 50px">
-        <button
-          on:click={() => (showIncommingTransactions = true)}
-          class="chart-category"
-          style="width: 10px; height: 10px; background-color: #0eff6b; border: none; "
-          id="incomming"
-        />
-        <label class="chart-category" for="incomming" style="margin-right: 8px">incomming</label>
-
-        <button
-          on:click={() => (showIncommingTransactions = false)}
-          class="chart-category"
-          style="width: 10px; height: 10px; background-color: #5a8bdb; border: none; "
-          id="outgoing"
-        />
-        <label class="chart-category" for="outgoing">outgoing</label>
-      </div>
-
-      <div style="margin-left: 40px;">
-        {#if showIncommingTransactions}
-          <div
-            style="text-align: center; position: absolute; left: 0; right: 0; top: 25px; margin: auto; font-family: Roboto Mono, monospace; font-size: 14px;"
-          >
-            <LinkedLabel linked="incomming" />
-          </div>
-          <div>
-            <LinkedChart
-              values={incommingTransactions}
-              labels={incommingDates}
-              linked="incomming"
-              valuePosition="floating"
-              showValue={true}
-              fill="#0eff6b"
-              tabindex="0"
-              height="300"
-              width="440"
-              align="left"
-              gap="4"
-              valueAppend="xkr"
-              grow={true}
-            />
-          </div>
-        {:else}
-          <div
-            style="text-align: center; position: absolute; left: 0; right: 0; top: 25px; margin: auto; font-family: Roboto Mono, monospace; font-size: 14px;"
-          >
-            <LinkedLabel linked="outgoing" />
-          </div>
-          <div>
-            <LinkedChart
-              values={outgoingTransactions}
-              labels={outgoingDates}
-              linked="outgoing"
-              valuePosition="floating"
-              showValue={true}
-              fill="#5a8bdb"
-              tabindex="0"
-              height="300"
-              width="440"
-              align="left"
-              gap="4"
-              valueAppend="xkr"
-              grow={true}
-            />
-          </div>
-        {/if}
-      </div>
-    {/if}
+    <div id="transactions-chart" style="margin-top:80px" />
   </div>
 </div>
 
@@ -165,10 +141,5 @@
   }
   .card-wrapper {
     margin: 10px;
-  }
-
-  .chart-category:hover {
-    cursor: pointer;
-    filter: brightness(140%);
   }
 </style>
