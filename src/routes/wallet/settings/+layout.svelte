582c7b165aa9c3b1e0b487799fb460c761a22d44
n9lsjr <nilsjr@protonmail.com>
2023-11-12 20:35:18 +0100

    Removed General from options, updated kryptokrona-backend-js in package.json

--- src/routes/wallet/settings/+layout.svelte
+++ src/routes/wallet/settings/+layout.svelte
@@ -1,21 +1,33 @@
 <script>
-  import Button from "$lib/components/buttons/Button.svelte";
-  import { fade } from "svelte/transition";
-  import { goto } from "$app/navigation";
-  import { page } from "$app/stores";
+  import Button from '$lib/components/buttons/Button.svelte';
+  import { fade } from 'svelte/transition';
+  import { goto } from '$app/navigation';
+  import { page } from '$app/stores';
 </script>
 
 <div class="header">
   <h3 in:fade>Settings</h3>
   <div in:fade>
-    <Button text="General" on:click={() => goto('/wallet/settings/general')} active={$page.url.pathname === '/wallet/settings/general'}/>
-    <Button text="Node" on:click={() => goto('/wallet/settings/node')} active={$page.url.pathname === '/wallet/settings/node'}/>
-    <Button text="Wallet" on:click={() => goto('/wallet/settings/wallet')} active={$page.url.pathname === '/wallet/settings/wallet'}/>
-    <Button text="Themes" on:click={() => goto('/wallet/settings/themes')} active={$page.url.pathname === '/wallet/settings/themes'}/>
+    <!-- <Button text="General" on:click={() => goto('/wallet/settings/general')} active={$page.url.pathname === '/wallet/settings/general'}/> -->
+    <Button
+      text="Node"
+      on:click={() => goto('/wallet/settings/node')}
+      active={$page.url.pathname === '/wallet/settings/node'}
+    />
+    <Button
+      text="Wallet"
+      on:click={() => goto('/wallet/settings/wallet')}
+      active={$page.url.pathname === '/wallet/settings/wallet'}
+    />
+    <Button
+      text="Themes"
+      on:click={() => goto('/wallet/settings/themes')}
+      active={$page.url.pathname === '/wallet/settings/themes'}
+    />
   </div>
 </div>
 <div class="wrapper">
-  <slot/>
+  <slot />
 </div>
 
 <style lang="scss">
@@ -26,7 +38,7 @@
     width: 100%;
     min-height: 60px;
     border-bottom: 1px solid var(--border-color);
-    padding: 0 2rem 0 2rem
+    padding: 0 2rem 0 2rem;
   }
 
   .wrapper {
