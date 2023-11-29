fb1bfc2b5d25a56551c557e7befea4e2b59b4d8b
f-r00t <fruktstav@gmail.com>
2023-11-29 16:10:34 +0100

    Added lightweight charts

--- src/routes/wallet/dashboard/+page.svelte
+++ src/routes/wallet/dashboard/+page.svelte
@@ -3,6 +3,7 @@
   import { onMount } from 'svelte';
   import Transaction from '$lib/components/layout/Transaction.svelte';
   import { goto } from '$app/navigation';
+  import { createChart } from 'lightweight-charts';
 
   const MAX_PAGES = 2;
   let transactions = [];
@@ -16,76 +17,57 @@
   });
 
   async function renderchart() {
-    var options = {
-      series: [
-        {
-          data: transactions.map(function (t) {
-            if (t.amount > 0) return (t.amount / 100000).toFixed(3);
-            return 0;
-          }),
-          name: 'Incomming',
-        },
-        {
-          data: transactions.map(function (t) {
-            if (t.amount < 0) return ((t.amount / 100000) * -1).toFixed(3);
-            return 0;
-          }),
-          name: 'Outgoing',
-        },
-      ],
-      labels: dates,
-      chart: {
-        type: 'bar',
-        height: '300',
-        width: '100%',
-        toolbar: {
-          show: false,
-        },
-      },
-      colors: ['#0eff6b', '#5a8bdb'],
-      plotOptions: {
-        bar: {
-          horizontal: false,
-          borderRadius: 2,
-          borderRadiusApplication: 'end',
-        },
-      },
-      fill: {
-        type: 'solid',
-      },
-      tooltip: {
-        enabled: true,
+    let data = [];
+    let runningBalance = 0.0;
+    for (const tx in transactions) {
+      const thisTx = transactions[tx];
+      runningBalance += thisTx.amount;
+      let dateFormatted = new Date(thisTx.time * 1000).toISOString().split('T')[0];
+      let formattedTx = { time: dateFormatted, value: runningBalance / 100000 };
+      data.push(formattedTx);
+    }
+
+    const summarizedData = data.reduce((acc, curr) => {
+      const existingItem = acc.find((item) => item.time === curr.time);
+
+      if (existingItem) {
+        existingItem.value += curr.value;
+      } else {
+        acc.push({ time: curr.time, value: curr.value });
+      }
+
+      return acc;
+    }, []);
+
+    const chart = createChart(document.getElementById('transactions-chart'), {
+      layout: {
+        background: { color: '#00000000' },
+        textColor: '#ffffff55',
       },
       grid: {
-        show: false,
+        vertLines: { color: '#00000000' },
+        horzLines: { color: '#00000000' },
       },
-      dataLabels: {
-        enabled: false,
-      },
-      legend: {
-        show: true,
-      },
-      xaxis: {
-        labels: {
-          show: false,
-        },
-        axisTicks: {
-          show: false,
+      rightPriceScale: {
+        scaleMargins: {
+          bottom: 0.15,
         },
       },
-      yaxis: {
-        opposite: true,
-        labels: {
-          formatter: (value) => {
-            return value + ' xkr';
-          },
-        },
-      },
-    };
+    });
+    console.log(data);
+    const areaSeries = chart.addAreaSeries({
+      topColor: '#2962FF',
+      bottomColor: 'rgba(41, 98, 255, 0.28)',
+      lineColor: '#2962FF',
+      lineWidth: 2,
+      crossHairMarkerVisible: false,
+    });
 
-    const ApexCharts = (await import('apexcharts')).default;
-    chart = new ApexCharts(document.querySelector('#transactions-chart'), options);
-    chart.render();
+    areaSeries.priceScale().applyOptions({ visible: false });
+    // areaSeries.timeScale().applyOptions({ visible: false });
+
+    areaSeries.setData(summarizedData);
+    chart.timeScale().fitContent();
   }
   async function getTransactions(transactions, pageNum) {
     let startIndex = pageNum * 10;
@@ -103,6 +85,7 @@
     transactions.reverse();
     dates = transactions.map((t) => new Date(t.time * 1000).toLocaleString());
     dates = dates;
+    console.log(transactions);
   }
 </script>
 
@@ -112,6 +95,7 @@
       <h3 in:fade>Dashboard</h3>
     </div>
     {#if dates.length > 0}
+      <div id="transactions-chart" style="width: 100%; height: 300px" />
       <div class="transactions">
         {#each latestFour as tx}
           <div class="row" on:click={() => goto(`/wallet/transaction/${tx.hash}`)}>
@@ -124,7 +108,6 @@
           </div>
         {/each}
       </div>
-      <div id="transactions-chart" />
     {/if}
   </div>
 </div>
