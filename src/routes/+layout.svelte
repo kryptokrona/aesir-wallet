a22197a5abc4e3c71ece6770f162ce81b1021f19
n9lsjr <nilsjr@protonmail.com>
2023-12-02 20:38:06 +0100

    Add success/err notification

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,43 +1,42 @@
 <script>
-  import "../lib/theme/global.scss";
-  import "../lib/theme/themes.scss";
-  import { onMount } from "svelte";
-  import TopBar from "../lib/components/layout/TopBar.svelte";
-  import toast, { Toaster } from "svelte-french-toast";
-  import { theme, user } from "$lib/stores/user.js";
-  import { goto } from "$app/navigation";
-  import { updater } from "$lib/stores/updater.js";
-  import { page } from "$app/stores";
-  import Updater from "$lib/components/updater/Updater.svelte";
-  import { hyper } from "$lib/stores/hyper.js";
+  import '../lib/theme/global.scss';
+  import '../lib/theme/themes.scss';
+  import { onMount } from 'svelte';
+  import TopBar from '../lib/components/layout/TopBar.svelte';
+  import toast, { Toaster } from 'svelte-french-toast';
+  import { theme, user } from '$lib/stores/user.js';
+  import { goto } from '$app/navigation';
+  import { updater } from '$lib/stores/updater.js';
+  import { page } from '$app/stores';
+  import Updater from '$lib/components/updater/Updater.svelte';
+  import { hyper } from '$lib/stores/hyper.js';
 
   let ready = false;
 
   onMount(async () => {
-
     document.documentElement.className = $theme;
 
     ready = true;
 
-    window.api.receive("updater", (data) => {
+    window.api.receive('updater', (data) => {
       data = data.toString();
 
       switch (data) {
-        case "checking":
+        case 'checking':
           //Do something
           break;
-        case "available":
+        case 'available':
           $updater.step = 1;
           $updater.showPopup = true;
           $updater.updateAvailable = true;
           break;
-        case "not-available":
-          if ($page.url.pathname === "/settings") {
+        case 'not-available':
+          if ($page.url.pathname === '/settings') {
             $updater.step = 4;
             $updater.showPopup = true;
           }
           break;
-        case "downloaded":
+        case 'downloaded':
           $updater.showPopup = true;
           $updater.step = 3;
           break;
@@ -46,7 +45,7 @@
       }
     });
 
-    window.api.receive("update-progress", (progress) => {
+    window.api.receive('update-progress', (progress) => {
       $updater.step = 2;
       $updater.percentageDownloaded = progress.percent;
       $updater.dataDownloaded = progress.transferred;
@@ -54,35 +53,49 @@
       $updater.downloadSpeed = progress.bytesPerSecond;
     });
 
-    window.api.receive("wrong-password", async () => {
-      toast.error("Wrong password", {
-        position: "top-right",
-        style: "border-radius: 5px; background: #171717; border: 1px solid #252525; color: #fff;"
+    window.api.receive('wrong-password', async () => {
+      toast.error('Wrong password', {
+        position: 'top-right',
+        style: 'border-radius: 5px; background: #171717; border: 1px solid #252525; color: #fff;',
       });
     });
 
-    window.api.receive("hyper-peer", peer => {
+    window.api.receive('error-message', async (error) => {
+      toast.error(error, {
+        position: 'top-right',
+        style: 'border-radius: 5px; background: #171717; border: 1px solid #252525; color: #fff;',
+      });
+    });
+
+    window.api.receive('success-message', async (success) => {
+      toast.success(success, {
+        position: 'top-right',
+        style:
+          'border-radius: 5px; background: var(--toast-bg-color); border: 1px solid var(--toast-b-color); color: var(--toast-text-color);',
+      });
+    });
+
+    window.api.receive('hyper-peer', (peer) => {
       if (peer) $hyper.peer++;
       if (!peer) $hyper.peer--;
     });
 
-    window.api.receive("hyper-message", (data) => {
-      if (!$page.url.pathname.startsWith("/wallet/hyper")) {
-        toast.success("New message", {
-          icon: "💬",
-          position: "top-right",
-          style: "border-radius: 5px; background: var(--toast-bg-color); border: 1px solid var(--toast-b-color); color: var(--toast-text-color);"
+    window.api.receive('hyper-message', (data) => {
+      if (!$page.url.pathname.startsWith('/wallet/hyper')) {
+        toast.success('New message', {
+          icon: '💬',
+          position: 'top-right',
+          style:
+            'border-radius: 5px; background: var(--toast-bg-color); border: 1px solid var(--toast-b-color); color: var(--toast-text-color);',
         });
       }
       let knownMessages = $hyper.messages ?? [];
       knownMessages.push(data);
       $hyper.messages = knownMessages;
     });
-
   });
 
-  $:if ($user.idleTime >= 300) goto("/auth/login-wallet");
-
+  $: if ($user.idleTime >= 300) goto('/auth/login-wallet');
 </script>
 
 <TopBar />
