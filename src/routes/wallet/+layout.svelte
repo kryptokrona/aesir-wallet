ecd852f606a32f8b59c51c783b786e959f4971a3
n9lsjr <nilsjr@protonmail.com>
2024-11-02 23:44:14 +0100

    Get balance from localstorage if set

--- src/routes/wallet/+layout.svelte
+++ src/routes/wallet/+layout.svelte
@@ -12,8 +12,10 @@
     $node.walletBlockCount = data.walletBlockCount;
 
     //Check to avoid triggering UI update if the balance is the same
-    if (data.balance[0] != localStorage.getItem('balance')) {
-      $wallet.balance = data.balance;
+    if (localStorage.getItem('balance')) {
+      if (data.balance[0] != localStorage.getItem('balance')) {
+        $wallet.balance = data.balance;
+      }
     }
 
     $user.idleTime = data.idle ?? 0;
